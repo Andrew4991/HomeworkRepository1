1eb9f35eb8ad5cd17a5799241deff62d65ffe744
Andrew <uglik94@gmail.com>
2021-04-28 23:40:12 +0300

    using Dictionary

--- BankApplicationPractice/BankLibrary/Bank.cs
+++ BankApplicationPractice/BankLibrary/Bank.cs
@@ -6,9 +6,11 @@ namespace BankLibrary
 {
     public class Bank<T> where T : IAccount
     {
+        private const string KgkPassPhrase = "CleanUp";
+
         private readonly AccountsCollection<T> _accounts = new();
 
-        private readonly Dictionary<KeyBankCell, BankCell<int>> _bankCell = new();
+        private readonly Dictionary<KeyLocker, object> _lockers = new();
 
         public void OpenAccount(OpenAccountParameters parameters)
         {
@@ -49,30 +51,53 @@ namespace BankLibrary
             }
         }
 
-        public void OpenCell(int data, string key, Action<string> handler)
+        public int AddLocker(string keyword, object data)
         {
-            var newCell = new BankCell<int>()
-            {
-                Id = _bankCell.Count,
-                Data = data,
-                _handlerCell = handler
-            };
+            var keyLocker = new KeyLocker(_lockers.Count + 1, keyword);
 
-            newCell.Open();
+            _lockers.Add(keyLocker, data);
 
-            _bankCell[new KeyBankCell(_bankCell.Count, key)] = newCell;
+            return keyLocker.Id;
         }
 
-        public int GetCell(int id, string key)
+        public object GetLockerData(int id, string keyword)
         {
-            var cell = _bankCell[new KeyBankCell(id, key)];
-
-            if (cell == null)
+            foreach (var locker in _lockers)
             {
-                throw new InvalidOperationException($"There are no cell in the bank!");
+                if (locker.Key.Matches(id, keyword))
+                {
+                    return locker.Value;
+                }
             }
 
-            return cell.Data;
+            throw new ArgumentException($"Cannot find locker with ID: {id} or keyword does not match");
+        }
+
+        public TU GetLockerData<TU>(int id, string keyword)
+        {
+            var data = GetLockerData(id, keyword);
+
+            if (data == null)
+            {
+                throw new ArgumentException($"Locker with ID: {id} contains no data.");
+            }
+
+            return (TU)data;
+        }
+
+        public void VisitKgk(string passPhrase)
+        {
+            if (passPhrase.Equals(KgkPassPhrase))
+            {
+                foreach (var locker in _lockers)
+                {
+                    _lockers[locker.Key] = null;
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"The passPhrase({passPhrase}) is not correct.");
+            }
         }
 
         private void AssertValidType(OpenAccountParameters parameters)
---
+++ BankApplicationPractice/BankLibrary/KeyLocker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public class KeyLocker
+    {
+        private readonly string _keyword;
+        private readonly int _id;
+
+        public KeyLocker(int id, string keyword)
+        {
+            _id = id;
+            _keyword = keyword;
+        }
+
+        public int Id => _id;
+
+        public bool Matches(int id, string keyword)
+        {
+            return _id == id && _keyword.Equals(keyword);
+        }
+
+        public override int GetHashCode()
+        {
+            return _keyword.GetHashCode() ^ Id.GetHashCode(); 
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as KeyLocker);
+        }
+
+        public bool Equals(KeyLocker obj)
+        {
+            return obj != null && obj._id == _id && obj._keyword.Equals(_keyword);
+        }
+    }
+}
